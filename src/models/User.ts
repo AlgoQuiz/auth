7b29c048e22d3ead2abb8da4981b7fa56146d0ff
peter-keller <keller.f.peter@gmail.com>
2021-05-28 20:16:27 +0100

    feat: User model extended, jwt generation

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,6 @@
+import jwt, { Secret } from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface UserInterface extends Document {
@@ -5,25 +8,52 @@ export interface UserInterface extends Document {
   email: string;
   password: string;
   id: string;
+  matchPassword: (password: any) => any;
 }
 
 const UserSchema: Schema = new Schema({
-  name: {
+  username: {
     type: String,
-    required: true,
+    required: [true, "Please provide username"],
   },
   email: {
     type: String,
-    required: true,
+    required: [true, "Please provide email address"],
+    unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
   },
   password: {
     type: String,
-    required: true,
-  },
-  id: {
-    type: String,
+    required: [true, "Please add a password"],
+    minlength: 6,
+    select: false,
   },
 });
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  // @ts-ignore
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.matchPassword = async function (password) {
+  // @ts-ignore
+  return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET as Secret, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 const User = mongoose.model<UserInterface>("User", UserSchema);
 
