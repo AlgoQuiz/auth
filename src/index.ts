e02e30739372af96e8bab673f3829eb5d77cb088
peter-keller <keller.f.peter@gmail.com>
2021-05-28 09:07:46 +0100

    feat: errorhandler and clsoe connection on error

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import app from "./app";
 
 const port: number = 5000 || process.env.PORT;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.info(`Server running on ${port}`);
+});
+
+process.on("unhandledRejection", (err: Error, promise) => {
+  console.log(`Logged Error: ${err.message}`);
+  server.close(() => process.exit(1));
 });
