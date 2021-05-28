275f1a5200b0d88b1cd986c655551e356c0c0ac2
peter-keller <keller.f.peter@gmail.com>
2021-05-28 20:16:56 +0100

    feat: user controller and routes - login and register

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,60 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import crypto from "crypto";
 
-export const signIn = async (req: Request, res: Response) => {};
+import User from "../models/User";
+import { ErrorResponse } from "../utils";
 
-export const signUp = async (req: Request, res: Response) => {};
+const sendToken = (user: any, statusCode: any, res: any) => {
+  const token = user.getSignedJwtToken();
+  res.status(statusCode).json({ success: true, token });
+};
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+
+  try {
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return next(new ErrorResponse("Invalid credentials", 401));
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return next(new ErrorResponse("Invalid credentials", 401));
+    }
+
+    sendToken(user, 200, res);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const register = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const user = await User.create({
+      username,
+      email,
+      password,
+    });
+
+    sendToken(user, 200, res);
+  } catch (err) {
+    next(err);
+  }
+};
