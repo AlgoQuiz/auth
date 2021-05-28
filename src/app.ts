172945a623ba1c7f686849d55406a8348c7c7610
peter-keller <keller.f.peter@gmail.com>
2021-05-28 20:15:53 +0100

    feat: errorhandler middleware, connection to db and error reporting enabled

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express, { Application } from "express";
 import { config as dotenvConfig } from "dotenv";
 
+import connectDB from "./config/db";
 import UserRouter from "./routes/user";
+import { errorHandler } from "./middlewares";
 
 const app: Application = express();
 
 dotenvConfig();
+
+connectDB();
+app.use(express.json());
+
+app.use(errorHandler);
 
 app.use("/auth", UserRouter);
 
